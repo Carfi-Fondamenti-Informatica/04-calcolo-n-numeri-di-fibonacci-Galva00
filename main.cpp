345fff19bb8eac63f43508bafd40c17e535dd2d4
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-23 19:41:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int fibo (int num) {
+    if (num<=1) {
+        return num;
+    }
+        return fibo(num-1) + fibo(num-2);
+}
 int main() {
-   return 0;
+    int num;
+    cin >> num;
+    if (num<2) {
+        cout << "errore" <<endl;
+    } else {
+        for (int i=1; i<=num; i++) {
+            cout <<fibo(i)<<endl;
+        }
+    }
+    return 0;
 }
